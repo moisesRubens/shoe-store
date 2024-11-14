f199007a6d7096602d0c749a3dd17d53bf8a5d74
moisesRubens <moisesrubens.pro@gmail.com>
2024-11-14 02:02:58 -0300

    To testing file To create classes Costumer, Shoe and ShoeStore Implement algorithm in the Main class To create more options in the Main class

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,42 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
     do {
         println("SISTEMA DE CALÇADOS")
-        println("1 - COSTUMER \n2 - EMPLOYEE")
+        println("1 - COSTUMER \n2 - EMPLOYEE \n3 - EXIT")
         print("Choose an option: ")
-        val option = readln().toInt()
+        var option = readln().toInt()
 
         when(option) {
-            1 -> println("A")
-            2 -> println("B")
+            1 -> showOptionsCostumer()
+            2 -> showOptionsEmployee()
         }
     } while(option != 3)
+}
+
+fun showOptionsCostumer() {
+    println("1 - BUY \n2 - VIEW SHOES \n3 - REGISTER \n4 - EXIT")
+    val option = readln().toInt()
+
+    when(option) {
+        1 -> null
+        2 -> null
+        3 -> null
+        4 -> return
+        else -> null
+
+    }
+}
+
+fun showOptionsEmployee() {
+    println("1 - REGISTER SHOE \n2 - VIEW SHOE STOCK \n3 - VIEW COSTUMER STOCK \n4 - EXIT")
+    val option = readln().toInt()
+
+    when(option) {
+        1 -> null
+        2 -> null
+        3 -> null
+        4 -> return
+        else -> null
+    }
 }
